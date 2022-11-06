1919f4fd4deff134545a46961f4cb39407077364
tah-dah <markcfranz13@gmail.com>
2022-11-06 12:49:41 -0600

    what is between CW

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-//dna to rna codewars 
-fn dna_to_rna(dna: &str) -> String {
-    dna.replace("T", "U")
+//function takes two integers and returns an array of integeres between input parameters
+
+fn between(a: i16, b: i16) -> Vec<i16> {
+    (a..=b).collect()
+    //Vec::from_iter (a..=b);
 }
 
 fn main() {
-
+   
 }
