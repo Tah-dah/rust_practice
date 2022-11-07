3ec8a12f78721bf7265bdf1f1432edbe5381c5df
tah-dah <markcfranz13@gmail.com>
2022-11-07 17:01:14 -0600

    add 5 code wars

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
-fn quarter_of(month: u8) -> u8 {
-    match month {
-        1..=3 => 1,
-        4..=6 => 2,
-        7..=9 => 3,
-        10..=12 => 4,
-        _=> 0
-        
-    }
+fn add_five(x: i32) -> i32 {
+    x+5
 }
 
 fn main() {
-    println!("{:?}",quarter_of(3));
-    println!("{:?}",quarter_of(12));
-    println!("{:?}",quarter_of(5));
-    println!("{:?}",quarter_of(6));
+    println!("{:?}",add_five(3));
+    println!("{:?}",add_five(12));
+    println!("{:?}",add_five(5));
+    
 }
