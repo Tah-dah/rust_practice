a4b68aa6c776209dbdd63ea611d949f6104219b7
tah-dah <markcfranz13@gmail.com>
2022-11-14 20:06:48 -0600

    keep hydrated .5 l per hour

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-
-fn arr(n: usize) -> Vec<u32> {
-    (0 .. n as u32).collect()
+fn litres(t: f64) -> i32 {
+    t as i32 / 2
 }
 fn main() {
    
@@ -9,6 +8,11 @@ fn main() {
 
 #[test]
     fn example_tests() {
-        assert_eq!(arr(0), vec![]);
-        assert_eq!(arr(4), vec![0,1,2,3]);
+        assert_eq!(litres(2.), 1);
+        assert_eq!(litres(1.4), 0);
+        assert_eq!(litres(12.3), 6);
+        assert_eq!(litres(0.82), 0);
+        assert_eq!(litres(11.8), 5);
+        assert_eq!(litres(1787.), 893);
+        assert_eq!(litres(0.), 0);
     }
