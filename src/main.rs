882d36628c0ce69b0b9115b3bbe731e0fbef744d
tah-dah <markcfranz13@gmail.com>
2022-11-20 17:59:06 -0600

    abbrev variables

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn abbrev_name(name: &str) -> String {
-    name.split(' ')
-        .map(|x| x.chars().nth(0).unwrap().to_string().to_uppercase())
-        .collect::<Vec<_>>()
-        .join(".")
-  }
+    let mut names = name.split(" ");
+    let first = names.next().unwrap();
+    let second = names.next().unwrap();
+    return first[0..1].to_uppercase().to_string() + "." + &second[0..1].to_uppercase();
+}
 fn main() {
    
     
