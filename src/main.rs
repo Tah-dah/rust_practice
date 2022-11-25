827ace0bbebca3f41086fce5d76c619a9ab6d750
tah-dah <markcfranz13@gmail.com>
2022-11-24 19:50:51 -0600

    reverse

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-fn guess_blue(blue_start: u32, red_start: u32, blue_pulled: u32, red_pulled: u32) -> f32 {                                          
-    let blue = blue_start - blue_pulled;                                                                                            
-    let red = red_start - red_pulled;                                                                                               
-    let total = blue + red;                                                                                                         
-    (blue as f32 / total as f32)                                                                                           
-}                                                                                                                                   
+fn reverse_seq(n: u32) -> Vec<u32> {
+    (1..=n).rev().collect()
+}                                                                                                                                 
     
 fn main() {
    
@@ -13,8 +10,6 @@ fn main() {
 
 
 #[test]
-fn basic_tests() {
-    assert_eq!(guess_blue(5, 5, 2, 3), 0.6);
-    assert_eq!(guess_blue(5, 7, 4, 3), 0.2);
-    assert_eq!(guess_blue(12, 18, 4, 6), 0.4);
-  }}
+fn sample_test() {
+    assert_eq!(reverse_seq(5), [5, 4, 3, 2, 1].to_vec());
+}
