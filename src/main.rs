f7b3c1a984265e177cb20526885eea5c0a5c5111
tah-dah <markcfranz13@gmail.com>
2022-11-27 22:31:47 -0600

    countng sheep

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,19 @@
-fn switch_it_up(n: usize) -> &'static str {
-    match n {
-        1 => "One",
-        2 => "Two",
-        3 => "Three",
-        4 => "Four",
-        5 => "Five",
-        6 => "Six",
-        7 => "Seven",
-        8 => "Eight",
-        9 => "Nine",
-        _ => "Zero"
-    }
-}
+fn count_sheep(sheep: &[bool]) -> u8 {
+    sheep              // take the sheep array
+      .iter()          // turn it into an iterable
+      .filter(|&&x| x) // filter it by taking the values in the array and returning only the true ones
+      .count() as u8   // count all of the elements in the filtered array and return a u8
+  }
 fn main() {
    
     
 }
 
-//use super::html_special_chars;
+
 
 #[test]
-fn basic() {
-    assert_eq!(switch_it_up(1), "One");
-    assert_eq!(switch_it_up(2), "Two");
-    assert_eq!(switch_it_up(3), "Three");
-}
+fn returns_correct_sheep_count() {
+    assert_eq!(count_sheep(&[false]), 0);
+    assert_eq!(count_sheep(&[true]), 1);
+    assert_eq!(count_sheep(&[true, false]), 1);
+  }
