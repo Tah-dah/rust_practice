38e62960010c8b6233bb89ebf424e6b933bcfb29
tah-dah <markcfranz13@gmail.com>
2022-11-29 20:15:28 -0600

    returning strings

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-fn count_sheep(sheep: &[bool]) -> u8 {
-    sheep              // take the sheep array
-      .iter()          // turn it into an iterable
-      .filter(|&&x| x) // filter it by taking the values in the array and returning only the true ones
-      .count() as u8   // count all of the elements in the filtered array and return a u8
-  }
+fn greet(name: &str) -> String {
+  format!("Hello, {} how are you doing today?", name)
+}
+
 fn main() {
    
     
@@ -11,9 +9,14 @@ fn main() {
 
 
 
-#[test]
-fn returns_correct_sheep_count() {
-    assert_eq!(count_sheep(&[false]), 0);
-    assert_eq!(count_sheep(&[true]), 1);
-    assert_eq!(count_sheep(&[true, false]), 1);
-  }
+use super::*;
+
+    #[test]
+    fn basic_tests() {
+        assert_eq!(greet("Ryan"), "Hello, Ryan how are you doing today?");
+        assert_eq!(
+            greet("Shingles"),
+            "Hello, Shingles how are you doing today?"
+        );
+    }
+}
