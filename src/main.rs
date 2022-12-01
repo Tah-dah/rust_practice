3441ca836a3344d7ddb112da1be394f5d563a8ee
tah-dah <markcfranz13@gmail.com>
2022-11-30 22:30:11 -0600

    max min values

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-fn greet(name: &str) -> String {
-  format!("Hello, {} how are you doing today?", name)
+fn minimum(arr: &[i32]) -> i32 {
+    *arr.iter().min().unwrap()
+}
+
+fn maximum(arr: &[i32]) -> i32 {
+    *arr.iter().max().unwrap()
 }
 
 fn main() {
@@ -9,14 +13,24 @@ fn main() {
 
 
 
-use super::*;
+#[cfg(test)]
+mod tests {
+    use super::{minimum, maximum};
+    
+    const ERR_MSG: &str = "\nYour result (left) did not match the expected output (right)";
+
+    fn dotest(arr: &[i32], expected_min: i32, expected_max: i32) {
+        assert_eq!(minimum(arr), expected_min, "{ERR_MSG} with function minimum and arr = {arr:?}");
+        assert_eq!(maximum(arr), expected_max, "{ERR_MSG} with function maximum and arr = {arr:?}");
+    }
 
     #[test]
-    fn basic_tests() {
-        assert_eq!(greet("Ryan"), "Hello, Ryan how are you doing today?");
-        assert_eq!(
-            greet("Shingles"),
-            "Hello, Shingles how are you doing today?"
-        );
+    fn fixed_tests() {
+        dotest(&[-52, 56, 30, 29, -54, 0, -110], -110, 56);
+        dotest(&[42, 54, 65, 87, 0], 0, 87);
+        dotest(&[1, 2, 3, 4, 5, 10], 1, 10);
+        dotest(&[-1, -2, -3, -4, -5, -10, 534, 43, 2, 1, 3, 4, 5, 5, 443, 443, 555, 555], -10, 555);
+        dotest(&[9], 9, 9);
+        dotest(&[4,6,2,1,9,63,-134,566], -134, 566);
     }
 }
