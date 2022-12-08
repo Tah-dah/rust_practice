7b4989b20e123ae2b28f78cfc2c9709c878bcbcd
tah-dah <markcfranz13@gmail.com>
2022-12-07 22:44:48 -0600

    object orientated piracy

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
-fn greet() -> &'static str {
-    "hello world!"
-}
+struct Ship {
+    draft: u32,
+    crew: u32,
+  }
+  
+  impl Ship {
+      fn is_worth_it(&self) -> bool {
+          self.draft > 3 * self.crew / 2 + 20
+      }
+  }
 
 fn main() {
    
@@ -9,7 +16,27 @@ fn main() {
 
 
 
-#[test]
-fn test_greets_the_world() {
-    assert_eq!(greet(), "hello world!", "should return the correct message");
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    const ERR_MSG: &str = "\nYour result (left) did not match the expected output (right)";
+
+    fn dotest(draft: u32, crew: u32, expected: bool) {
+        let ship = Ship {
+            draft : draft,
+            crew : crew, 
+        };
+        assert_eq!(ship.is_worth_it(), expected, "{ERR_MSG} with draft = {draft}, crew = {crew}")   
+    }
+
+    #[test]
+    fn fixed_tests() {
+        dotest(0, 0, false);
+        dotest(15, 20, false);
+        dotest(35, 20, false);
+        dotest(100, 20, true);
+        dotest(29, 6, false);
+        dotest(30, 6, true);
+    }
 }
