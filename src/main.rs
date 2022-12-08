f38415f3741f14a9bc49a8285018552bfa292fbb
tah-dah <markcfranz13@gmail.com>
2022-12-07 22:45:42 -0600

    set alarm

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-struct Ship {
-    draft: u32,
-    crew: u32,
-  }
-  
-  impl Ship {
-      fn is_worth_it(&self) -> bool {
-          self.draft > 3 * self.crew / 2 + 20
-      }
-  }
+fn set_alarm(employed: bool, vacation: bool) -> bool {
+    employed && !vacation
+}
 
 fn main() {
    
@@ -19,24 +12,12 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
-    
-    const ERR_MSG: &str = "\nYour result (left) did not match the expected output (right)";
-
-    fn dotest(draft: u32, crew: u32, expected: bool) {
-        let ship = Ship {
-            draft : draft,
-            crew : crew, 
-        };
-        assert_eq!(ship.is_worth_it(), expected, "{ERR_MSG} with draft = {draft}, crew = {crew}")   
-    }
 
     #[test]
-    fn fixed_tests() {
-        dotest(0, 0, false);
-        dotest(15, 20, false);
-        dotest(35, 20, false);
-        dotest(100, 20, true);
-        dotest(29, 6, false);
-        dotest(30, 6, true);
+    fn test_set_alarm() {
+        assert_eq!(set_alarm(true, true), false, "Fails when input is true, true");
+        assert_eq!(set_alarm(false, true), false, "Fails when input is false, true");
+        assert_eq!(set_alarm(false, false), false, "Fails when input is false, false");
+        assert_eq!(set_alarm(true, false), true, "Fails when input is true, false");
     }
 }
