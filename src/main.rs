0235246344220c88d381405c784986efe8213d51
tah-dah <markcfranz13@gmail.com>
2022-12-08 23:29:12 -0600

    messi goals

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-fn get_char(c: i32) -> char {
-    c as u8 as char
-}
- 
+static la_liga_goals: u32 = 43;
+static champions_league_goals: u32 = 10;
+static copa_del_rey_goals: u32 = 5;
+
+static total_goals: u32 = la_liga_goals + champions_league_goals + copa_del_rey_goals;
 
 fn main() {
    
@@ -15,17 +16,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn example() {
-        assert_eq!(get_char(55), '7');
-        assert_eq!(get_char(56), '8');
-        assert_eq!(get_char(57), '9');
-        assert_eq!(get_char(58), ':');
-        assert_eq!(get_char(59), ';');
-        assert_eq!(get_char(60), '<');
-        assert_eq!(get_char(61), '=');
-        assert_eq!(get_char(62), '>');
-        assert_eq!(get_char(63), '?');
-        assert_eq!(get_char(64), '@');
-        assert_eq!(get_char(65), 'A');
+    fn test_add() {
+        assert_eq!(total_goals, 58, "total goals should equal to 58");
     }
 }
